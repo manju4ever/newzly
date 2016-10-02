a2b9dd6c7782e8c7f6e6b47b12be1837f8c41ebc
Manju <manju4ever@MANJUNATHs-MacBook-Pro.local>
2016-10-02 12:58:10 +0530

    Scoring

--- src/helpers/NewsSearchApi.js
+++ src/helpers/NewsSearchApi.js
@@ -2,6 +2,6 @@ import Axios from "axios";
 
 module.exports =  {
 	getFeedFor: function(searchKey) {
-		return Axios.get("https://webhose.io/search?token=c5e60f7d-d0f1-4072-94a3-6bc8f2808683&size=10&performance_score=5&q="+searchKey);
+		return Axios.get("https://webhose.io/search?token=c5e60f7d-d0f1-4072-94a3-6bc8f2808683&size=10&performance_score=8&q="+searchKey);
 	}
 };
